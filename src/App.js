e9da2a4c9d9eef454b40af9ad710fa9212921537
Vinivy Gabriel <vinivygabriel8@gmail.com>
2023-06-16 14:41:43 -0300

    Meu primeiro projeto em React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+//tudo oq esta aqui reflete no nosso sait, é como fosse o nosso HTml
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Olá doidos</h1>
+      <p>Meu primeiro app</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
